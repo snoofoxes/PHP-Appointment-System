a9658326d51be1dff363766d23a17be2bf171fb9
yskm <christian.petallar17@gmail.com>
2024-06-18 18:00:44 +0800

    first commit

---
+++ hms.sql
@@ -0,0 +1,431 @@
+-- phpMyAdmin SQL Dump
+-- version 5.2.0
+-- https://www.phpmyadmin.net/
+--
+-- Host: 127.0.0.1
+-- Generation Time: Jun 02, 2023 at 05:12 PM
+-- Server version: 10.4.24-MariaDB
+-- PHP Version: 8.1.6
+
+SET SQL_MODE = "NO_AUTO_VALUE_ON_ZERO";
+START TRANSACTION;
+SET time_zone = "+00:00";
+
+
+/*!40101 SET @OLD_CHARACTER_SET_CLIENT=@@CHARACTER_SET_CLIENT */;
+/*!40101 SET @OLD_CHARACTER_SET_RESULTS=@@CHARACTER_SET_RESULTS */;
+/*!40101 SET @OLD_COLLATION_CONNECTION=@@COLLATION_CONNECTION */;
+/*!40101 SET NAMES utf8mb4 */;
+
+--
+-- Database: `hms`
+--
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `admin`
+--
+
+CREATE TABLE `admin` (
+  `id` int(11) NOT NULL,
+  `username` varchar(255) NOT NULL,
+  `password` varchar(255) NOT NULL,
+  `updationDate` varchar(255) NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=latin1;
+
+--
+-- Dumping data for table `admin`
+--
+
+INSERT INTO `admin` (`id`, `username`, `password`, `updationDate`) VALUES
+(1, 'admin', 'petallar12', '30-05-2023 09:38:19 PM');
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `appointment`
+--
+
+CREATE TABLE `appointment` (
+  `id` int(11) NOT NULL,
+  `doctorSpecialization` varchar(255) DEFAULT NULL,
+  `doctorId` int(11) DEFAULT NULL,
+  `userId` int(11) DEFAULT NULL,
+  `appointmentDate` varchar(255) DEFAULT NULL,
+  `appointmentTime` varchar(255) DEFAULT NULL,
+  `postingDate` timestamp NULL DEFAULT current_timestamp(),
+  `userStatus` int(11) DEFAULT NULL,
+  `doctorStatus` int(11) DEFAULT NULL,
+  `updationDate` timestamp NULL DEFAULT NULL ON UPDATE current_timestamp()
+) ENGINE=InnoDB DEFAULT CHARSET=latin1;
+
+--
+-- Dumping data for table `appointment`
+--
+
+INSERT INTO `appointment` (`id`, `doctorSpecialization`, `doctorId`, `userId`, `appointmentDate`, `appointmentTime`, `postingDate`, `userStatus`, `doctorStatus`, `updationDate`) VALUES
+(1, 'ENT', 1, 1, '2022-11-10', '12:45 PM', '2022-11-06 12:21:48', 1, 0, '2022-11-06 12:23:35'),
+(2, 'ENT', 1, 2, '2022-11-17', '7:00 PM', '2022-11-06 13:16:18', 1, 1, NULL),
+(3, 'General practitioners', 4, 1, '2023-06-09', '6:30 PM', '2023-05-31 10:20:10', 0, 1, '2023-05-31 10:22:06'),
+(4, 'General practitioners', 4, 1, '2023-06-09', '6:30 PM', '2023-05-31 10:21:56', 1, 1, NULL);
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `doctors`
+--
+
+CREATE TABLE `doctors` (
+  `id` int(11) NOT NULL,
+  `specilization` varchar(255) DEFAULT NULL,
+  `doctorName` varchar(255) DEFAULT NULL,
+  `address` longtext DEFAULT NULL,
+  `contactno` bigint(11) DEFAULT NULL,
+  `docEmail` varchar(255) DEFAULT NULL,
+  `password` varchar(255) DEFAULT NULL,
+  `creationDate` timestamp NULL DEFAULT current_timestamp(),
+  `updationDate` timestamp NULL DEFAULT NULL ON UPDATE current_timestamp()
+) ENGINE=InnoDB DEFAULT CHARSET=latin1;
+
+--
+-- Dumping data for table `doctors`
+--
+
+INSERT INTO `doctors` (`id`, `specilization`, `doctorName`, `address`, `contactno`, `docEmail`, `password`, `creationDate`, `updationDate`) VALUES
+(4, 'General practitioners', 'Christian Dan Petallar', 'Imadejas Butuan City', 9098944600, 'christiandanpetallar@gmail.com', 'cf91dca06b4a50193434656fcdf93e34', '2023-05-30 16:00:59', NULL);
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `doctorslog`
+--
+
+CREATE TABLE `doctorslog` (
+  `id` int(11) NOT NULL,
+  `uid` int(11) DEFAULT NULL,
+  `username` varchar(255) DEFAULT NULL,
+  `userip` binary(16) DEFAULT NULL,
+  `loginTime` timestamp NULL DEFAULT current_timestamp(),
+  `logout` varchar(255) DEFAULT NULL,
+  `status` int(11) DEFAULT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=latin1;
+
+--
+-- Dumping data for table `doctorslog`
+--
+
+INSERT INTO `doctorslog` (`id`, `uid`, `username`, `userip`, `loginTime`, `logout`, `status`) VALUES
+(29, 4, 'christiandanpetallar@gmail.com', 0x3a3a3100000000000000000000000000, '2023-05-31 10:23:02', '31-05-2023 03:53:28 PM', 1),
+(30, 4, 'christiandanpetallar@gmail.com', 0x3a3a3100000000000000000000000000, '2023-06-02 14:29:18', NULL, 1);
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `doctorspecilization`
+--
+
+CREATE TABLE `doctorspecilization` (
+  `id` int(11) NOT NULL,
+  `specilization` varchar(255) DEFAULT NULL,
+  `creationDate` timestamp NULL DEFAULT current_timestamp(),
+  `updationDate` timestamp NULL DEFAULT NULL ON UPDATE current_timestamp()
+) ENGINE=InnoDB DEFAULT CHARSET=latin1;
+
+--
+-- Dumping data for table `doctorspecilization`
+--
+
+INSERT INTO `doctorspecilization` (`id`, `specilization`, `creationDate`, `updationDate`) VALUES
+(18, 'General practitioners', '2023-05-30 15:58:39', NULL),
+(19, 'Immunologists', '2023-05-30 15:58:55', NULL),
+(20, 'Pharmacists', '2023-05-30 15:59:11', NULL);
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `tblcontactus`
+--
+
+CREATE TABLE `tblcontactus` (
+  `id` int(11) NOT NULL,
+  `fullname` varchar(255) DEFAULT NULL,
+  `email` varchar(255) DEFAULT NULL,
+  `contactno` bigint(12) DEFAULT NULL,
+  `message` mediumtext DEFAULT NULL,
+  `PostingDate` timestamp NULL DEFAULT current_timestamp(),
+  `AdminRemark` mediumtext DEFAULT NULL,
+  `LastupdationDate` timestamp NULL DEFAULT NULL ON UPDATE current_timestamp(),
+  `IsRead` int(11) DEFAULT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=latin1;
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `tblmedicalhistory`
+--
+
+CREATE TABLE `tblmedicalhistory` (
+  `ID` int(10) NOT NULL,
+  `PatientID` int(10) DEFAULT NULL,
+  `BloodPressure` varchar(200) DEFAULT NULL,
+  `BloodSugar` varchar(200) NOT NULL,
+  `Weight` varchar(100) DEFAULT NULL,
+  `Temperature` varchar(200) DEFAULT NULL,
+  `MedicalPres` mediumtext DEFAULT NULL,
+  `CreationDate` timestamp NOT NULL DEFAULT current_timestamp() ON UPDATE current_timestamp()
+) ENGINE=InnoDB DEFAULT CHARSET=latin1;
+
+--
+-- Dumping data for table `tblmedicalhistory`
+--
+
+INSERT INTO `tblmedicalhistory` (`ID`, `PatientID`, `BloodPressure`, `BloodSugar`, `Weight`, `Temperature`, `MedicalPres`, `CreationDate`) VALUES
+(1, 1, '80/120', '120', '85', '98', 'Test', '2022-11-06 13:19:41');
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `tblpatient`
+--
+
+CREATE TABLE `tblpatient` (
+  `ID` int(10) NOT NULL,
+  `Docid` int(10) DEFAULT NULL,
+  `PatientName` varchar(200) DEFAULT NULL,
+  `PatientContno` bigint(10) DEFAULT NULL,
+  `PatientEmail` varchar(200) DEFAULT NULL,
+  `PatientGender` varchar(50) DEFAULT NULL,
+  `PatientAdd` mediumtext DEFAULT NULL,
+  `PatientAge` int(10) DEFAULT NULL,
+  `PatientMedhis` mediumtext DEFAULT NULL,
+  `CreationDate` timestamp NULL DEFAULT current_timestamp(),
+  `UpdationDate` timestamp NULL DEFAULT NULL ON UPDATE current_timestamp(),
+  `statusName` varchar(255) DEFAULT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=latin1;
+
+--
+-- Dumping data for table `tblpatient`
+--
+
+INSERT INTO `tblpatient` (`ID`, `Docid`, `PatientName`, `PatientContno`, `PatientEmail`, `PatientGender`, `PatientAdd`, `PatientAge`, `PatientMedhis`, `CreationDate`, `UpdationDate`, `statusName`) VALUES
+(34, 1, 'Christian Dan', 909894460, 'christiandanpetallar@gmail.com', 'male', 'asdadadadda', 21, 'N/A', '2023-05-30 15:33:37', NULL, 'chronicHIV'),
+(35, 4, 'Sheena Dalupere', 909090909, 'sheena@gmail.com', 'female', 'hhseergergrgrer', 22, 'sfhdfhddfdfhdfhdfh', '2023-06-02 14:34:03', NULL, 'AIDS');
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `tblstatus`
+--
+
+CREATE TABLE `tblstatus` (
+  `ID` int(10) NOT NULL,
+  `statusName` varchar(200) DEFAULT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
+
+--
+-- Dumping data for table `tblstatus`
+--
+
+INSERT INTO `tblstatus` (`ID`, `statusName`) VALUES
+(3, 'acuteHIV'),
+(4, 'chronicHIV'),
+(5, 'AIDS'),
+(6, 'deceased');
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `userlog`
+--
+
+CREATE TABLE `userlog` (
+  `id` int(11) NOT NULL,
+  `uid` int(11) DEFAULT NULL,
+  `username` varchar(255) DEFAULT NULL,
+  `userip` binary(16) DEFAULT NULL,
+  `loginTime` timestamp NULL DEFAULT current_timestamp(),
+  `logout` varchar(255) DEFAULT NULL,
+  `status` int(11) DEFAULT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=latin1;
+
+--
+-- Dumping data for table `userlog`
+--
+
+INSERT INTO `userlog` (`id`, `uid`, `username`, `userip`, `loginTime`, `logout`, `status`) VALUES
+(6, 1, 'christiandanpetallar@gmail.com', 0x3a3a3100000000000000000000000000, '2023-05-30 16:05:35', NULL, 1),
+(7, 1, 'christiandanpetallar@gmail.com', 0x3a3a3100000000000000000000000000, '2023-05-31 10:18:54', '31-05-2023 03:52:49 PM', 1),
+(8, 1, 'christiandanpetallar@gmail.com', 0x3a3a3100000000000000000000000000, '2023-06-02 14:28:28', '02-06-2023 07:59:05 PM', 1);
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `users`
+--
+
+CREATE TABLE `users` (
+  `id` int(11) NOT NULL,
+  `fullName` varchar(255) DEFAULT NULL,
+  `address` longtext DEFAULT NULL,
+  `city` varchar(255) DEFAULT NULL,
+  `gender` varchar(255) DEFAULT NULL,
+  `email` varchar(255) DEFAULT NULL,
+  `password` varchar(255) DEFAULT NULL,
+  `regDate` timestamp NULL DEFAULT current_timestamp(),
+  `updationDate` timestamp NULL DEFAULT NULL ON UPDATE current_timestamp()
+) ENGINE=InnoDB DEFAULT CHARSET=latin1;
+
+--
+-- Dumping data for table `users`
+--
+
+INSERT INTO `users` (`id`, `fullName`, `address`, `city`, `gender`, `email`, `password`, `regDate`, `updationDate`) VALUES
+(1, 'Christian Dan Petallar', 'Purok 5 A', 'Butuan', 'male', 'christiandanpetallar@gmail.com', 'cf91dca06b4a50193434656fcdf93e34', '2022-11-06 12:13:56', '2023-05-30 16:05:58'),
+(3, 'Sheena Dalupere', 'Purok 5, Brgy. Dumalagan', 'Butuan', 'female', 'sheena@gmail.com', 'cf91dca06b4a50193434656fcdf93e34', '2023-05-30 16:12:23', NULL);
+
+--
+-- Indexes for dumped tables
+--
+
+--
+-- Indexes for table `admin`
+--
+ALTER TABLE `admin`
+  ADD PRIMARY KEY (`id`);
+
+--
+-- Indexes for table `appointment`
+--
+ALTER TABLE `appointment`
+  ADD PRIMARY KEY (`id`);
+
+--
+-- Indexes for table `doctors`
+--
+ALTER TABLE `doctors`
+  ADD PRIMARY KEY (`id`);
+
+--
+-- Indexes for table `doctorslog`
+--
+ALTER TABLE `doctorslog`
+  ADD PRIMARY KEY (`id`);
+
+--
+-- Indexes for table `doctorspecilization`
+--
+ALTER TABLE `doctorspecilization`
+  ADD PRIMARY KEY (`id`);
+
+--
+-- Indexes for table `tblcontactus`
+--
+ALTER TABLE `tblcontactus`
+  ADD PRIMARY KEY (`id`);
+
+--
+-- Indexes for table `tblmedicalhistory`
+--
+ALTER TABLE `tblmedicalhistory`
+  ADD PRIMARY KEY (`ID`);
+
+--
+-- Indexes for table `tblpatient`
+--
+ALTER TABLE `tblpatient`
+  ADD PRIMARY KEY (`ID`);
+
+--
+-- Indexes for table `tblstatus`
+--
+ALTER TABLE `tblstatus`
+  ADD PRIMARY KEY (`ID`);
+
+--
+-- Indexes for table `userlog`
+--
+ALTER TABLE `userlog`
+  ADD PRIMARY KEY (`id`);
+
+--
+-- Indexes for table `users`
+--
+ALTER TABLE `users`
+  ADD PRIMARY KEY (`id`),
+  ADD KEY `email` (`email`);
+
+--
+-- AUTO_INCREMENT for dumped tables
+--
+
+--
+-- AUTO_INCREMENT for table `admin`
+--
+ALTER TABLE `admin`
+  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=2;
+
+--
+-- AUTO_INCREMENT for table `appointment`
+--
+ALTER TABLE `appointment`
+  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=5;
+
+--
+-- AUTO_INCREMENT for table `doctors`
+--
+ALTER TABLE `doctors`
+  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=5;
+
+--
+-- AUTO_INCREMENT for table `doctorslog`
+--
+ALTER TABLE `doctorslog`
+  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=31;
+
+--
+-- AUTO_INCREMENT for table `doctorspecilization`
+--
+ALTER TABLE `doctorspecilization`
+  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=21;
+
+--
+-- AUTO_INCREMENT for table `tblcontactus`
+--
+ALTER TABLE `tblcontactus`
+  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=3;
+
+--
+-- AUTO_INCREMENT for table `tblmedicalhistory`
+--
+ALTER TABLE `tblmedicalhistory`
+  MODIFY `ID` int(10) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=2;
+
+--
+-- AUTO_INCREMENT for table `tblpatient`
+--
+ALTER TABLE `tblpatient`
+  MODIFY `ID` int(10) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=36;
+
+--
+-- AUTO_INCREMENT for table `tblstatus`
+--
+ALTER TABLE `tblstatus`
+  MODIFY `ID` int(10) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=7;
+
+--
+-- AUTO_INCREMENT for table `userlog`
+--
+ALTER TABLE `userlog`
+  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=9;
+
+--
+-- AUTO_INCREMENT for table `users`
+--
+ALTER TABLE `users`
+  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=4;
+COMMIT;
+
+/*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
+/*!40101 SET CHARACTER_SET_RESULTS=@OLD_CHARACTER_SET_RESULTS */;
+/*!40101 SET COLLATION_CONNECTION=@OLD_COLLATION_CONNECTION */;
